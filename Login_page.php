ac9fbbae603ad7ea5673dc29b7c5fce5dc5fe674
AQwebber <124896681+AQwebber@users.noreply.github.com>
2023-02-09 22:37:36 +0800

    Aqwebber practice

---
+++ Login_page.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html>
+<head>
+<title></title>
+
+
+
+
+
+
+
+</head>
+<body>
+
+
+<form action="" method="POST">
+<input type="email" name="email"
+placeholder="Enter your email"/>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
---
+++ example.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html>
+<head>
+<title></title>
+
+
+
+
+
+
+
+</head>
+
+<body>
+
+
+<form action="" method="POST">
+
+<input type="email" name="email"
+placeholder="Enter your email"/>
+
+
+
+
+
+</form>
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
